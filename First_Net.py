03f6b9a4b738a44819ee9fc8884c00cbbfa92313
pranav bhasin <pranav.bhasin@students.iiit.ac.in>
2017-09-24 00:42:00 +0530

    Commit before Final

--- First_Net.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-import keras
-from keras.model import Sequential
-from keras.layers import Dense,Dropout,Flatten
-from keras import backend
-from parse_data import parse_data()
-
-X_train, Y_train, X_test, Y_test = parse_data()
